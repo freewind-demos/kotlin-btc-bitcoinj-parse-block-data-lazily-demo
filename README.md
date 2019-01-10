ba15d70041a2954628dc36f19d316fb2a010d0df
freewind <nowindlee@gmail.com>
2019-01-10 21:26:39 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Kotlin Btc bitcoinj Parse Block Data Demo
-=========================================
+Kotlin Btc bitcoinj Parse Block Data Lazily Demo
+================================================
+
+When there is a lot of data, lazily parsing is very important.
 
 Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -4,6 +4,7 @@ import org.bitcoinj.core.Context
 import org.bitcoinj.params.MainNetParams
 import org.bitcoinj.utils.BlockFileLoader
 import java.io.File
+import kotlin.sequences.asSequence
 
 val mainNetParams = MainNetParams()
 val blockChainFiles = listOf(File("./btc-data/blocks/blk00000.dat"))
@@ -11,8 +12,31 @@ val blockChainFiles = listOf(File("./btc-data/blocks/blk00000.dat"))
 fun main(args: Array<String>) {
     Context.getOrCreate(mainNetParams)
 
+    loading()
+    lazyLoading()
+
+}
+
+private fun loading() {
+    println("----------- loading ------------")
     val loader = BlockFileLoader(mainNetParams, blockChainFiles)
-    for (block in loader) {
-        System.out.println(block)
+    loader.mapIndexed { index, block ->
+        System.out.println("---- $index ------")
+        index to block.hash
+    }.take(10).forEach { (index, hash) ->
+        println("$index: $hash")
     }
+}
+
+private fun lazyLoading() {
+    println("----------- lazyLoading ------------")
+    val loader = BlockFileLoader(mainNetParams, blockChainFiles)
+    loader.asSequence()
+            .mapIndexed { index, block ->
+                System.out.println("---- $index ------")
+                index to block.hash
+            }.take(10)
+            .forEach { (index, hash) ->
+                println("$index: $hash")
+            }
 }
